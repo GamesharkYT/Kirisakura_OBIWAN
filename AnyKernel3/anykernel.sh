e42ced4c4cb6f8ccaf4186ff0538a90bae731db2
jaat4u <devangsingh811@gmail.com>
2021-06-23 17:23:52 +0000

    le bsdk mar le

---
+++ AnyKernel3/anykernel.sh
@@ -0,0 +1,46 @@
+# AnyKernel3 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Gayme Sarak Kernel - ROG 3
+do.devicecheck=0
+do.modules=1
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=
+device.name2=
+device.name3=
+device.name4=
+device.name5=
+supported.versions=10
+supported.patchlevels=2020-12 -
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+set_perm_recursive 0 0 750 750 $ramdisk/*;
+
+## AnyKernel install
+dump_boot;
+
+# remove old root patch avoidance hack
+patch_cmdline "skip_override" "";
+
+write_boot;
+
+## end install
+
+	ui_print " "; ui_print "Kernel Flashed Successfully. Reboot & Enjoy!";
+
